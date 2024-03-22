b25c0faed9c7207e59d21db66bf8548687a29769
Ritabrata Bhattcharya <ritabrata.bhatta@tigeranalytics.com>
2024-03-22 19:38:42 +0530

    assignment 5

---
+++ production/scripts.py
@@ -0,0 +1,10 @@
+"""Module for listing down additional custom functions required for production.
+
+author: ritabrata.bhattacharya
+"""
+
+import pandas as pd
+
+def binned_selling_price(df):
+    """Bin the selling price column using quantiles."""
+    return pd.qcut(df["unit_price"], q=10)
